23b2d5f0b79e6f02b19a2ff4e9db0230f901f274
dinkha-phan <73351060+dinkha-phan@users.noreply.github.com>
2021-06-19 23:39:45 +0700

    homepage-ver1

--- source/app.js
+++ source/app.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+//logger
 app.use(morgan('dev'));
 
+
+// view
+app.engine('hbs', exphbs({
+  defaultLayout: 'main.hbs'
+}));
+app.set('view engine', 'hbs');
+
+
+// url processing
 app.use(express.urlencoded({
   extended: true
 }));
 
-const PORT = 3000;
+
+app.use('/assets', express.static('assets'))
+
+
+//route
+app.get('/', function (req, res) {
+    // res.render('home');
+    res.render('home', )
+})
+
+
+
+//listening at PORT...
+const PORT = 3000
 app.listen(PORT, function () {
-  console.log(`EC Web App listening at http://localhost:${PORT}`);
-});
+    console.log(`WebMKD app listening at http://localhost:${PORT}`)
+})

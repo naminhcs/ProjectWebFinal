36f774b8aa20b0544f423dca056069fb95ac5b3a
dinkha-phan <73351060+dinkha-phan@users.noreply.github.com>
2021-06-23 16:47:07 +0700

    register-ver2 + login-ver1 (front-ent)

--- source/app.js
+++ source/app.js
@@ -4,7 +4,6 @@ const exphbs = require('express-handlebars');
 var hbs_sections = require('express-handlebars-sections');
 const moment = require('moment');
 
-
 const app = express();
 
 //logger
@@ -33,10 +32,11 @@ app.use('/assets', express.static('assets'))
 //route
 app.get('/', function (req, res) {
   res.render('home');
+
 })
 
-app.get('/post_detail', function (req, res) {
-  res.render('post_detail')
+app.get('/post-detail', function (req, res) {
+  res.render('post-detail')
 })
 
 app.get('/register', function (req, res) {
@@ -47,7 +47,19 @@ app.post('/register', function (req, res) {
   console.log(req.body)
 })
 
+app.get('/is-available-username', function (req, res) {
+  const username = req.query.username;
+  //Kiem tra co ton tai account voi username = req.query.username hay chua
+})
 
+
+app.get('/login', function (req, res) {
+  res.render('login')
+})
+
+app.post('/login', function (req, res) {
+  //check username/email + password
+})
 
 //listening at PORT...
 const PORT = 3000

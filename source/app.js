8614acfc6526d7c899a6e9168404ec79243bb658
naminhcs <naminhcs@gmail.com>
2021-07-09 01:38:35 +0700

    testing

--- source/app.js
+++ source/app.js
@@ -4,6 +4,22 @@ const exphbs = require('express-handlebars');
 var hbs_sections = require('express-handlebars-sections');
 const session = require('express-session');
 const dotenv = require('dotenv');
+const fireStorage = require('firebase/storage')
+global.XMLHttpRequest = require("xhr2");
+// const db = require('./db');
+const configDatabase = {
+  apiKey: "AIzaSyBplWJSzjQIXRp3EnxGjVCWV1rHMvhVV2g",
+  authDomain: "web-app-484e0.firebaseapp.com",
+  projectId: "web-app-484e0",
+  storageBucket: "web-app-484e0.appspot.com",
+  messagingSenderId: "734047821097",
+  appId: "1:734047821097:web:d0fcb7b647cf88e9f46b38",
+  measurementId: "G-QM2G2JLBQ8"
+}
+const firebase = require('firebase');
+firebase.initializeApp(configDatabase);
+
+
 dotenv.config();
 
 const app = express();
@@ -38,9 +54,17 @@ app.use(session({
   saveUninitialized: true
 }))
 
-require('./middlewares/userMiddle')(app);
-require('./middlewares/confirmationMiddle')(app);
+// require('./middlewares/userMiddle')(app);
+// require('./middlewares/confirmationMiddle')(app);
 
+
+// test upload file and load file
+async function getURL(){
+  var storageRef = firebase.storage().ref();
+  var urlDownloadLink = await storageRef.child('/avatar.png').getDownloadURL();
+  console.log(urlDownloadLink);
+}
+getURL();
 //listening at PORT...
 const PORT = 3000
 app.listen(PORT, function () {

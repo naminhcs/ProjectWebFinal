f8885e8a97635c52c7baf4a7d930155ba0cb7072
dinhkha-phan <phandinhkha00@gmail.com>
2021-08-10 02:19:39 +0700

    (FE) - Pagination - list post

--- source/controllers/categoryRoute.js
+++ source/controllers/categoryRoute.js
@@ -9,79 +9,153 @@ router.use(bodyParser.json());
 // util function
 function processingNumberOfPagesToView(nPages, page) { // needing to get total pages of cat
     var page_numbers = [];
-    for (i = 1; i <= nPages; i++) {
-        page_numbers.push({
-            value: i,
-            isCurrent: i === +page
-        });
+    nPages = Math.round(nPages)
+    page = +page
+
+    if (nPages <= 6) {
+        for (i = 1; i <= nPages; i++) {
+            page_numbers.push({
+                value: i,
+                isCurrent: i === +page
+            });
+        }
+    } else {
+
+        if (page < 3) {
+            // 3 pages dau
+            for (i = 1; i <= 3; i++) {
+                page_numbers.push({
+                    value: i,
+                    isCurrent: i === +page
+                });
+            }
+            page_numbers.push({
+                isThreeDots: true,
+            });
+            // 2 pages cuoi
+            for (i = nPages - 1; i <= nPages; i++) {
+                page_numbers.push({
+                    value: i,
+                    isCurrent: i === +page
+                });
+            }
+        } else {
+            if (page >= nPages - 4) {
+                if (page == nPages - 4) {
+                    for (i = nPages - 5; i <= nPages; i++) {
+                        page_numbers.push({
+                            value: i,
+                            isCurrent: i === +page
+                        });
+                    }
+
+                } else {
+                    // 5 pages cuoi
+                    for (i = nPages - 4; i <= nPages; i++) {
+                        page_numbers.push({
+                            value: i,
+                            isCurrent: i === +page
+                        });
+                    }
+                }
+            } else {
+                // console.log(page)
+                // console.log(nPages)
+                for (i = page - 1; i <= page + 1; i++) {
+                    page_numbers.push({
+                        value: i,
+                        isCurrent: i === +page
+                    });
+                }
+                page_numbers.push({
+                    isThreeDots: true,
+                });
+
+                for (i = nPages - 2; i <= nPages; i++) {
+                    page_numbers.push({
+                        value: i,
+                        isCurrent: i === +page
+                    });
+                }
+            }
+        }
     }
-    return page_numbers
+    // console.log(page_numbers)
+    return {page_numbers}
 }
 
 // get 10post/page    (category2)
 
-async function getPostPremiumByCat2(cat2, page){
+async function getPostPremiumByCat2(cat2, page) {
     var ans = await postModel.getPostPremiumByCat2(cat2, page)
     var stringAns = JSON.stringify(Object.assign({}, ans));
     var jsonAns = JSON.parse(stringAns)
     const cnt = await postModel.getPagePremium(cat2);
     var nPages = cnt / 10;
     if (cnt % 10 !== 0) nPages++;
+    var paging = processingNumberOfPagesToView(nPages, page);
     const obj = {
         data: jsonAns,
         isEmpty: jsonAns.length,
-        page_numbers: processingNumberOfPagesToView(nPages, page),
+        page_numbers: paging.page_numbers,
+        pageCurrent: page,
         nameCat1: jsonAns['0']['nameCat1'],
-        nameCat2: jsonAns['0']['nameCat2']
+        nameCat2: jsonAns['0']['nameCat2'],
     }
     return obj
 }
 
-async function getPostByCat2(cat2, page){
+async function getPostByCat2(cat2, page) {
     var ans = await postModel.getPostByCat2(cat2, page);
     var stringAns = JSON.stringify(Object.assign({}, ans));
     var jsonAns = JSON.parse(stringAns)
     const cnt = await postModel.getPage(cat2)
     var nPages = cnt / 10;
     if (cnt % 10 !== 0) nPages++;
+    var paging = processingNumberOfPagesToView(nPages, page);
     const obj = {
         data: jsonAns,
         isEmpty: jsonAns.length,
-        page_numbers: processingNumberOfPagesToView(nPages, page),
+        page_numbers: paging.page_numbers,
+        pageCurrent: page,
         nameCat1: jsonAns['0']['nameCat1'],
-        nameCat2: jsonAns['0']['nameCat2']
+        nameCat2: jsonAns['0']['nameCat2'],
     }
     return obj
 }
 
-async function getPostPremiumByCat1(cat1, page){
+async function getPostPremiumByCat1(cat1, page) {
     var ans = await postModel.getPostPremiumByCat1(cat1, page)
     var stringAns = JSON.stringify(Object.assign({}, ans));
     var jsonAns = JSON.parse(stringAns)
     const cnt = await postModel.getPagePremium(cat1);
     var nPages = cnt / 10;
     if (cnt % 10 !== 0) nPages++;
+    var paging = processingNumberOfPagesToView(nPages, page);
     const obj = {
         data: jsonAns,
         isEmpty: jsonAns.length,
-        page_numbers: processingNumberOfPagesToView(nPages, page),
-        nameCat1: jsonAns['0']['nameCat1']
+        page_numbers: paging.page_numbers,
+        pageCurrent: page,
+        nameCat1: jsonAns['0']['nameCat1'],
     }
     return obj
 }
 
-async function getPostByCat1(cat1, page){
+async function getPostByCat1(cat1, page) {
     var ans = await postModel.getPostByCat1(cat1, page);
     var stringAns = JSON.stringify(Object.assign({}, ans));
     var jsonAns = JSON.parse(stringAns)
     const cnt = await postModel.getPage(cat1)
     var nPages = cnt / 10;
     if (cnt % 10 !== 0) nPages++;
+    var paging = processingNumberOfPagesToView(nPages, page)
     const obj = {
         data: jsonAns,
         isEmpty: jsonAns.length,
-        page_numbers: processingNumberOfPagesToView(nPages, page),
-        nameCat1: jsonAns['0']['nameCat1']
+        page_numbers: paging.page_numbers,
+        pageCurrent: page,
+        nameCat1: jsonAns['0']['nameCat1'],
     }
     return obj;
 }
@@ -93,12 +167,12 @@ router.get('/:cat1/:cat2', async function (req, res) {
     const page = req.query.page || 1
     var obj = {}
     var permission = 0;
-    if (typeof(req.session.data) === 'undefined'){
+    if (typeof (req.session.data) === 'undefined') {
         permission = 0
     } else permission = 1;
-    
+
     console.log(permission)
-    if (permission){
+    if (permission) {
         obj = await getPostPremiumByCat2(cat2, page)
     } else {
         obj = await getPostByCat2(cat2, page)
@@ -115,12 +189,12 @@ router.get('/:cat1', async function (req, res) {
     const page = req.query.page || 1
     var obj = {}
     var permission = 0;
-    if (typeof(req.session.data) === 'undefined'){
+    if (typeof (req.session.data) === 'undefined') {
         permission = 0
     } else permission = 1;
 
     console.log(permission)
-    if (permission){
+    if (permission) {
         obj = await getPostPremiumByCat1(cat1, page)
     } else obj = await getPostByCat1(cat1, page)
     // res.send(jsonAns)

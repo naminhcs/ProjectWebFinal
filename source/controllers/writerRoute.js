47faf412610c6042e6b23fe550e099686035cf4b
dinhkha-phan <phandinhkha00@gmail.com>
2021-08-19 13:44:34 +0700

    (FE) - alert when save/submit/delete

--- source/controllers/writerRoute.js
+++ source/controllers/writerRoute.js
@@ -46,7 +46,9 @@ router.post('/add/save', auth.isWriter, upload.single('urlPic'), async function 
     } else {
         var post = await saveModel.getPostByID(id, 'SavePost')
         if (post.userWriter !== req.session.data.userName) {
-            res.send('You dont have permission for edit this post')
+            req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+            res.redirect('/writer/view/writing-post')
+            //res.send('You dont have permission for edit this post')
             return;
         }
     }
@@ -54,7 +56,9 @@ router.post('/add/save', auth.isWriter, upload.single('urlPic'), async function 
     if (!req.file) file = null;
     else file = req.file
     const result = await saveModel.savePostByID(id, data, file)
-    res.send(result)
+    req.session.successMsg = 'Đã lưu bài báo'
+    res.redirect('/writer/view/writing-post')
+    //res.send(result)
 })
 
 router.post('/add/submit', auth.isWriter, upload.single('urlPic'), async function (req, res) {
@@ -66,7 +70,9 @@ router.post('/add/submit', auth.isWriter, upload.single('urlPic'), async functio
     } else {
         var post = await saveModel.getPostByID(id, 'SavePost')
         if (post.userWriter !== req.session.data.userName) {
-            res.send('You dont have permission for edit this post')
+            req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+            res.redirect('/writer/view/draft-post')
+            //res.send('You dont have permission for edit this post')
             return;
         }
     }
@@ -74,7 +80,10 @@ router.post('/add/submit', auth.isWriter, upload.single('urlPic'), async functio
     if (!req.file) file = null;
     else file = req.file
     const result = await saveModel.submitPost(id, data, file)
-    res.send(result)
+
+    req.session.successMsg = 'Đã nộp bài báo để duyệt!'
+    res.redirect('/writer/view/draft-post')
+    // res.send(result)
 })
 
 // ======================================== writing-post(SAVE) ============================================
@@ -84,11 +93,17 @@ router.get('/view/writing-post', auth.isWriter, async function (req, res) {
     obj = await saveModel.getPostByWriter(req.session.data.userName, page, 'SavePost')
     var nPages = await saveModel.getTotalPage('SavePost', req.session.data.userName)
 
+    res.locals.successMsg = req.session.successMsg;
+    req.session.successMsg = ''
+
+    res.locals.errorMsg = req.session.errorMsg;
+    req.session.errorMsg = ''
+
     res.render('vwWriter/writing/writing-posts', {
         layout: 'writer.hbs',
         db: obj,
         totalPage: nPages,
-        page:page,
+        page: page,
         isWritingPosts: true,
     });
 })
@@ -98,7 +113,9 @@ router.get('/edit/writing-post', auth.isWriter, async function (req, res) {
     var id = req.query.id;
     const post = await saveModel.getPostByID(id, 'SavePost')
     if (post.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/draft-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
 
@@ -115,11 +132,15 @@ router.post('/del/writing-post/:id', auth.isWriter, async function (req, res) {
     var id = req.params.id;
     const post = await saveModel.getPostByID(id, 'SavePost')
     if (post.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/writing-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
     const result = await saveModel.delelteSavePost(id, 'SavePost')
-    res.send(result)
+    req.session.successMsg = 'Đã xóa bài báo'
+    res.redirect('/writer/view/writing-post')
+    //res.send(result)
 })
 
 
@@ -130,6 +151,13 @@ router.get('/view/draft-post', auth.isWriter, async function (req, res) {
     var obj = await saveModel.getPostByWriter(req.session.data.userName, page, 'DrafPost')
     var nPages = await saveModel.getTotalPage('DrafPost', req.session.data.userName)
     //console.log('list post: ', obj)
+
+    res.locals.successMsg = req.session.successMsg;
+    req.session.successMsg = ''
+
+    res.locals.errorMsg = req.session.errorMsg;
+    req.session.errorMsg = ''
+
     res.render('vwWriter/draft/draft-posts', {
         layout: 'writer.hbs',
         db: obj,
@@ -145,7 +173,9 @@ router.get('/edit/draft-post', auth.isWriter, async function (req, res) {
     var obj;
     obj = await saveModel.getPostByID(id, 'DrafPost')
     if (obj.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/draft-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
 
@@ -163,25 +193,33 @@ router.post('/edit/draft-post', auth.isWriter, upload.single('urlPic'), async fu
     var obj;
     obj = await saveModel.getPostByID(id, 'DrafPost')
     if (obj.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/draft-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
     var file = null;
     if (!req.file) file = null;
     else file = req.file
     const result = await draftModel.editDraftPost(id, data, file);
-    res.send(result)
+    req.session.successMsg = 'Đã nộp bài báo để duyệt!'
+    res.redirect('/writer/view/draft-post')
+    //res.send(result)
 })
 
 router.post('/del/draft-post/:id', auth.isWriter, async function (req, res) {
     var id = req.params.id;
     const obj = await saveModel.getPostByID(id, 'DrafPost')
     if (obj.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/draft-post')
+        //res.send('you dont have permisson to edit this post')
         return;
     }
     const result = await saveModel.delelteSavePost(id, 'DrafPost')
-    res.send(result)
+    req.session.successMsg = 'Đã nộp bài báo để duyệt!'
+    res.redirect('/writer/view/draft-post')
+    // res.send(result)
 })
 
 //===============================================Reject-post ==============================================
@@ -189,6 +227,13 @@ router.get('/view/reject-post', auth.isWriter, async function (req, res) {
     page = req.query.page || 1
     const obj = await saveModel.getPostByWriter(req.session.data.userName, page, 'RejectPost')
     const nPages = await saveModel.getTotalPage('RejectPost', req.session.data.userName)
+
+    res.locals.successMsg = req.session.successMsg;
+    req.session.successMsg = ''
+
+    res.locals.errorMsg = req.session.errorMsg;
+    req.session.errorMsg = ''
+
     res.render('vwWriter/reject/reject-posts', {
         layout: 'writer.hbs',
         db: obj,
@@ -202,7 +247,9 @@ router.get('/edit/reject-post', auth.isWriter, async function (req, res) {
     id = req.query.id
     const obj = await saveModel.getPostByID(id, 'RejectPost')
     if (obj.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/reject-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
     res.render('vwWriter/reject/edit-reject-post', {
@@ -220,14 +267,18 @@ router.post('/edit/reject-post/save/:id', auth.isWriter, upload.single('urlPic')
     var post = await rejectModel.getRejectPostByID(id)
     console.log(post)
     if (post.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/reject-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
     var file = null;
     if (!req.file) file = null;
     else file = req.file
     const result = await rejectModel.editRejectPost(id, body, file)
-    res.send(result)
+    req.session.successMsg = 'Đã lưu bài báo!'
+    res.redirect('/writer/view/reject-post')
+    // res.send(result)
 })
 
 // after edit, submit post to drafPost
@@ -236,25 +287,33 @@ router.post('/edit/reject-post/submit/:id', auth.isWriter, upload.single('urlPic
     const id = req.params.id
     var post = await rejectModel.getRejectPostByID(id)
     if (post.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/reject-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
     var file;
     if (!req.file) file = null;
     else file = req.file
     const result = await rejectModel.submitRejectPost(id, body, file)
-    res.send(result)
+    req.session.successMsg = 'Đã nộp để lưu bài báo!'
+    res.redirect('/writer/view/reject-post')
+    // res.send(result)
 })
 
 router.post('/del/reject-post/:id', auth.isWriter, async function (req, res) {
     const id = req.params.id
     var post = await rejectModel.getRejectPostByID(id)
     if (post.userWriter !== req.session.data.userName) {
-        res.send('you dont have permisson to edit this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/reject-post')
+        // res.send('you dont have permisson to edit this post')
         return;
     }
     const result = await saveModel.delelteSavePost(id, 'RejectPost')
-    res.send(result)
+    req.session.successMsg = 'Đã xóa bài báo!'
+    res.redirect('/writer/view/reject-post')
+    // res.send(result)
 })
 
 
@@ -263,6 +322,13 @@ router.get('/view/waiting-post', auth.isWriter, async function (req, res) {
     page = req.query.page || 1
     obj = await saveModel.getPostByWriter(req.session.data.userName, page, 'WaitingPost')
     var nPages = await saveModel.getTotalPage('WaitingPost', req.session.data.userName)
+
+    res.locals.successMsg = req.session.successMsg;
+    req.session.successMsg = ''
+
+    res.locals.errorMsg = req.session.errorMsg;
+    req.session.errorMsg = ''
+
     res.render('vwWriter/waiting/waiting-posts', {
         layout: 'writer.hbs',
         db: obj,
@@ -276,7 +342,9 @@ router.get('/preview/waiting-post/:id', auth.isWriter, async function (req, res)
     id = req.params.id
     var obj = await saveModel.getPostByID(id, 'WaitingPost')
     if (obj.userWriter !== req.session.data.userName) {
-        res.send('you dont have permission to read this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/waiting-post')
+        // res.send('you dont have permission to read this post')
         return;
     }
     res.render('vwWriter/waiting/view-wating-post.hbs', {
@@ -293,6 +361,12 @@ router.get('/view/public', auth.isWriter, async function (req, res) {
     obj = await saveModel.getPostByWriter(req.session.data.userName, page, 'Post')
     var nPages = await saveModel.getTotalPage('Post', req.session.data.userName)
 
+    res.locals.successMsg = req.session.successMsg;
+    req.session.successMsg = ''
+
+    res.locals.errorMsg = req.session.errorMsg;
+    req.session.errorMsg = ''
+
     res.render('vwWriter/public/public-posts', {
         layout: 'writer.hbs',
         db: obj,
@@ -306,7 +380,9 @@ router.get('/view/public/:id', auth.isWriter, async function (req, res) {
     id = req.params.id
     var obj = await saveModel.getPostByID(id, 'Post')
     if (obj.userWriter !== req.session.data.userName) {
-        res.send('you dont have permission to read this post')
+        req.session.errorMsg = 'Bạn không có quyền truy cập vào bài báo!'
+        res.redirect('/writer/view/waiting-post')
+        // res.send('you dont have permission to read this post')
         return;
     }
     res.render('vwWriter/public/view-public-post', {
